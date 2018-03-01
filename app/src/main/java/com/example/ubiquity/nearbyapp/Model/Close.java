826a8093961d1498f51cefeaeee461db0942115a
burcutopcu <burcutopcu96@gmail.com>
2018-03-01 20:18:00 +0300

    POJO Classes

---
+++ app/src/main/java/com/example/ubiquity/nearbyapp/Model/Close.java
@@ -0,0 +1,38 @@
+package com.example.ubiquity.nearbyapp.Model;
+
+/**
+ * Created by Ubiquity on 2/28/2018.
+ */
+
+public class Close {
+
+    private String time;
+
+    private String day;
+
+    public String getTime ()
+    {
+        return time;
+    }
+
+    public void setTime (String time)
+    {
+        this.time = time;
+    }
+
+    public String getDay ()
+    {
+        return day;
+    }
+
+    public void setDay (String day)
+    {
+        this.day = day;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [time = "+time+", day = "+day+"]";
+    }
+}
---
+++ app/src/main/java/com/example/ubiquity/nearbyapp/Model/Periods.java
@@ -0,0 +1,38 @@
+package com.example.ubiquity.nearbyapp.Model;
+
+/**
+ * Created by Ubiquity on 2/28/2018.
+ */
+
+public class Periods {
+
+    private Open open;
+
+    private Close close;
+
+    public Open getOpen ()
+    {
+        return open;
+    }
+
+    public void setOpen (Open open)
+    {
+        this.open = open;
+    }
+
+    public Close getClose ()
+    {
+        return close;
+    }
+
+    public void setClose (Close close)
+    {
+        this.close = close;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [open = "+open+", close = "+close+"]";
+    }
+}
